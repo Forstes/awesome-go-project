5a4d7a10a4e14e12b328342fe2c57aea8496b51e
mrowa223 <88025828+mrowa223@users.noreply.github.com>
2022-11-12 03:36:32 +0600

    Update main.js

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -6,3 +6,24 @@ for (var i = 0; i < navLinks.length; i++) {
 		break;
 	}
 }
+
+//Add function add image for form Page
+function updateImage() {
+  console.log(this.files);
+  if (this.files && this.files.length) {
+    preview.src = window.URL.createObjectURL(this.files[0]);
+    preview.setAttribute("height", "100%");
+  } else {
+    preview.setAttribute("height", "32px");
+    preview.src = "photo.svg";
+  }
+}
+
+const input = document.getElementById("avatar");
+const preview = document.getElementById("preview");
+
+input.addEventListener("change", updateImage);
+
+
+
+
